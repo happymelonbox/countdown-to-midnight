fd791ce527e5c4dbe0105c6eba8ccc4180438496
Justin Berger Howes <howes.j.j@gmail.com>
2021-08-17 23:21:20 +1000

    commit tree 023a11561a8090e3f3db9e47b718f04a5586a039

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+    while seconds > 0
+        puts "#{seconds} Second(s)!".upcase
+        seconds -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(sec)
+    while sec > 0
+        puts "#{sec} Second(s)!".upcase
+        sleep 1
+        sec -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+countdown_with_sleep(5)
